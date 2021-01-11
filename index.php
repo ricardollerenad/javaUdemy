11989e087c09e868b889cc839d8440a042facead
ricardollerenad <ricardo.llerena.d@gmail.com>
2021-01-11 18:11:37 -0500

    3er intento del index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php
-<h1>Hola mundo como estas!!!!</h1>
-?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hola mundo como estan todos</h1>
+</body>
+</html>
